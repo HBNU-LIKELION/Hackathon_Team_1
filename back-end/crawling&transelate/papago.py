5002b483d8c6485d3dc509d69f3d65fffa086f46
yeachan <dus001228@naver.com>
2023-07-20 15:57:16 +0900

    add japan

--- back-end/crawling&transelate/papago.py
+++ back-end/crawling&transelate/papago.py
@@ -1,7 +1,10 @@
 import requests
 import json
 import urllib.request
-from secret import client_id, client_secret
+from secret import *
+
+client_id = "STM1RaKY9idSeW0QaxTz"
+client_secret = "DGclz7YbwM"
 
 def get_translate(text):
     lang = get_lang(text)

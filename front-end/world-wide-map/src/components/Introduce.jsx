bc7e0a7a6c193c24cddcd07cda7829b8c26a8d5a
junoshon <sanoldfox@naver.com>
2023-07-31 17:55:01 +0900

    Feat : 서비스 소개 화면 테스트

---
+++ front-end/world-wide-map/src/components/Introduce.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import earth from "../assets/지구본1.png";
+import earthBackground from "../assets/지구배경.png";
+import "../App.css";
+import "./introduce.css"
+import styled from "styled-components";
+
+
+export default function Introduce() {
+  const Button = styled.button`
+    font-family: 'PyeongChangPeace-Bold';
+    width: 50%;
+    padding: 13px;
+    border: none;
+    background-color: #66CCC5;
+    color: white;
+    text-align: center;
+    cursor: pointer;
+    border-radius: 7px;
+    font-size: 20px;
+    &:hover{
+      background-color: #8de3dd;
+    }
+    
+  
+  `;
+  
+  return (
+    
+    // style={{
+    // //   backgroundImage : `url(${earthBackground})`,
+    // //   backgroundPosition: 'center',
+    // //   backgroundRepeat: 'no-repeat',
+    // //   backgroundSize: 'cover',
+    // //
+    // // }}
+    
+    <div className="my-component">
+      <br/>
+      <h1 style={{color:"white"}}>World Wide Hot Topic!</h1>
+      
+      <div id="box">
+        
+        <h3 style={{color:"white"}}>전 세계 실시간 뉴스들을 한눈에 모아보세요!</h3>
+        
+        <img src={earth} width="300px" className="App-logo"/>
+      
+      </div>
+      <Button>
+        <b>시작하기</b>
+      </Button>
+    </div>
+  
+  );
+}

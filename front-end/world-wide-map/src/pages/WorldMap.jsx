d2d61dea07c1fe6c8d6917790bf3373f06e0a452
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-08-08 20:23:30 +0900

    feat: Card 컴포넌트 출력 맟 슬라이드 기능 추가

--- front-end/world-wide-map/src/pages/WorldMap.jsx
+++ front-end/world-wide-map/src/pages/WorldMap.jsx
@@ -19,7 +19,15 @@ export default function WorldMap() {
       background-repeat: repeat;
     `;
 
+    const CardsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  `;
+
     const [newsData, setNewsData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const cardsPerPage = 6;
 
     useEffect(() => {
         async function fetchData() {
@@ -29,16 +37,32 @@ export default function WorldMap() {
         fetchData();
     }, []);
 
+    const totalPageCount = Math.ceil(newsData.length / cardsPerPage);
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => (prevPage === totalPageCount ? 1 : prevPage + 1));
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage((prevPage) => (prevPage === 1 ? totalPageCount : prevPage - 1));
+    };
+
+    const startIndex = (currentPage - 1) * cardsPerPage;
+    const endIndex = startIndex + cardsPerPage;
 
     return (
         <div>
             <WorldMapStyle>
-                <img src={worldMap} alt=""/>
+                <img src={worldMap} alt="" />
             </WorldMapStyle>
             <h1>그 외 기사들</h1>
-            <div>
-                {newsData.length > 0 && <Card data={newsData[0]} />}
-            </div>
+            <CardsContainer>
+                {newsData.slice(startIndex, endIndex).map((data) => (
+                    <Card key={data.id} data={data} />
+                ))}
+            </CardsContainer>
+            <button onClick={handlePrevPage}>이전</button>
+            <button onClick={handleNextPage}>다음</button>
         </div>
     );
 }

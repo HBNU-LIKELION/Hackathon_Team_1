3df1ef2f7f84e1eebebcef44347e37cca465f3b5
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-08-08 18:11:49 +0900

    refactor: Card 컴포넌트 구조 및 기능 수정

--- front-end/world-wide-map/src/components/Card.jsx
+++ front-end/world-wide-map/src/components/Card.jsx
@@ -1,61 +1,63 @@
 import React, {Fragment} from 'react';
 import styled from 'styled-components';
+import {truncateTitle} from "../utils/truncateTitle";
+import {getCountryFlag} from "../utils/getCountryFlag";
 
-export default function Card({data, itemStyle, truncateTitle, getCountryFlag}) {
-  const CardContainer = styled.div`
-    margin-top: 90px;
-    margin-left: 80px;
-    display: grid;
-    grid-template-columns: 0.75fr 0.75fr 0.75fr;
-    gap: 5px 5px;
-  `;
+export default function Card({data}) {
+    const CardItem = styled.div`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      max-width: 400px;
+      height: 450px;
+      text-align: center;
+      border-radius: 7px;
+      border: 2px solid black;
+      position: relative;
+      transition: transform 0.75s;
 
-  const CardItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 300px;
-    height: 350px;
-    text-align: center;
-    border-radius: 7px;
-    border: 2px solid black;
-    position: relative;
+      &:hover {
+        transform: translateY(-20px);
+      }
+    `;
 
-    &:hover {
-      transform: translateY(-20px);
-      transition: 0.75s;
-    }
-  `;
+    const IconWrapper = styled.div`
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      z-index: 2;
+      font-size: 70px;
+      border: none;
+    `;
 
-  const CardImage = styled.img`
-    width: 100%;
-    height: 50%;
-    object-fit: cover;
-    border-radius: 7px 7px 0 0;
-  `;
+    const CardImage = styled.img`
+      width: 100%;
+      height: 85%;
+      object-fit: cover;
+      border-radius: 7px 7px 0 0;
+      z-index: 1;
+    `;
 
-  const CardContent = styled.div`
-    height: 50%;
-    padding: 10px;
-  `;
+    const CardContent = styled.div`
+      height: 15%;
+      padding: 10px;
+      font-size: 2.3vw; // 글자 크기를 viewport 너비의 3%로 설정하는 과정
+    `;
 
-  return (
+    //TODO : src의 alt 속성에 들어갈 이미지 필요
+    return (
+        <Fragment>
+            <CardItem key={data.id}>
+                <IconWrapper>{getCountryFlag(data.country)}</IconWrapper>
+                <CardImage src={data.img} alt="추후 수정 필요"/>
+                <CardContent>
+                    {truncateTitle(data.title)}
+                    <br />
+                    <span style={{ fontSize: "17px" }}>국가 : {data.country}</span>
+                </CardContent>
 
-    <Fragment>
-      <CardContainer>
-        {data.map((item) => (
-          <CardItem key={item.id} style={itemStyle}>
-            <CardImage src={item.img} alt=""/>
-            <CardContent>
-              <h2>
-                {getCountryFlag(item.country)} {truncateTitle(item.title, 10)}
-              </h2>
-              <p>Country: {item.country}</p>
-            </CardContent>
-          </CardItem>
-        ))}
-      </CardContainer>
-    </Fragment>
-  );
-
+            </CardItem>
+        </Fragment>
+    );
 }

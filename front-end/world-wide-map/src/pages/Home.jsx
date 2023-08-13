6d4483d4d4bc504aa9499d434785aeb8e6ded41d
junoshon <sanoldfox@naver.com>
2023-08-14 01:53:13 +0900

    feat: 세계지도 소개 이후 카드 소개 추가 및 서비스 소개 멘트 변경

--- front-end/world-wide-map/src/pages/Home.jsx
+++ front-end/world-wide-map/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import styled, {keyframes} from "styled-components";
 import {useNavigate} from "react-router-dom";
 import Modal from "../components/Modal";
 import minimap from "../assets/miniWorldMapImage.png";
+import miniCardImage from "../assets/miniCardImage.png"
+import FirstModal from "../components/FirstModal";
+import SecondModal from "../components/SecondModal";
 
 
-
-const rotate = keyframes` // 애니메이션은 컴포넌트 위에 선언
+const rotate = keyframes`
   from {
     transform: rotateY(0deg);
   }
@@ -17,24 +19,32 @@ const rotate = keyframes` // 애니메이션은 컴포넌트 위에 선언
 `;
 export default function Home() {
   
+  const navigate = useNavigate();
+  
   //TODO: Home에서 Modal.jsx를 렌더링하는 로직 변경 필요
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const navigate = useNavigate();
   
   const handleOpenModal = () => {
     setIsModalOpen(true);
   };
-  
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  
   const handleModalButtonClick = () => {
+    setIsModalOpen2(true);
+  };
+  
+  
+  //두번째 모달을 위한 코드
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  const handleCloseModal2 = () => {
+    setIsModalOpen2(false);
+  };
+  const handleModalButtonClick2 = () => {
     navigate("/worldmap");
   };
   
   const Button = styled.button`
-
     width: 50%;
     padding: 13px;
     border: none;
@@ -44,12 +54,9 @@ export default function Home() {
     cursor: pointer;
     border-radius: 7px;
     font-size: 20px;
-
     &:hover {
       background-color: #8de3dd;
     }
-  
-  
   `;
   
   const Background = styled.div`
@@ -62,9 +69,9 @@ export default function Home() {
   `;
   
   const RotatingImage = styled.img` // rotate 애니메이션을 적용할 이미지 컴포넌트
-  animation: ${rotate} 4s linear infinite; // 애니메이션 속성 설정
-  width: 300px;
-`;
+    animation: ${rotate} 4s linear infinite; // 애니메이션 속성 설정
+    width: 300px;
+  `;
   
   
   return (
@@ -72,31 +79,35 @@ export default function Home() {
     <div className="my-component">
       
       <Background>
-        <h1 style={{color: "white", paddingTop: "80px"}}>World Wide Hot Topic!</h1>
-        
         <div id="box">
-          <h3 style={{color: "white"}}>전 세계 실시간 뉴스들을 한눈에 모아보세요!</h3>
-          {/*<img src={globe} width="300px" alt=""/>*/}
-          <RotatingImage src={globe} alt="Rotating Globe" />
+          <p style={{color: "white", paddingTop: "80px", fontStyle: "italic", fontSize: "48px"}}><font
+            style={{color: "#66CCC5"}}>N</font>ews <font style={{color: "#66CCC5"}}>B</font>road <font
+            style={{color: "#66CCC5"}}>A</font>nywhere !</p>
+          <RotatingImage src={globe} alt="Rotating Globe"/>
         </div>
         
         <Button onClick={() => {
           handleOpenModal();
         }}>
-          <b>서비스 소개 보기</b>
+          <b>How to use?</b>
         </Button>
+        
         <Modal
           isOpen={isModalOpen}
           onClose={handleCloseModal}
           onButtonClick={handleModalButtonClick}
-          buttonText="더 보기"
+          buttonText="다음으로"
         >
-          <h2>전 세계의 실시간 핫 토픽을 체험해보세요!</h2>
-          <p style={{fontWeight: "bold"}}>NBA는 News Broad Anywhere의 약자로, 전 세계의 실시간 뉴스들을 한 눈에 모아볼 수 있는 비영리 서비스입니다.</p>
-          <p style={{fontWeight: "bold"}}>세계적으로 핫한 기사들이 지도에 Pin으로 고정되어 있습니다.</p>
-          <p style={{fontWeight: "bold"}}>클릭해서 펼쳐보세요!</p>
-          <img src={minimap} alt=""/>
+          <FirstModal/>
+        </Modal>
         
+        <Modal
+          isOpen={isModalOpen2}
+          onClose={handleCloseModal2}
+          onButtonClick={handleModalButtonClick2}
+          buttonText="시작하기"
+        >
+          <SecondModal/>
         </Modal>
       </Background>
     </div>

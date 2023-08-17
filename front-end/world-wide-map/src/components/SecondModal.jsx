17a1c539ce60537df1624cd7cc6d538c1f9acbbc
junoshon <sanoldfox@naver.com>
2023-08-17 22:14:07 +0900

    fix: Second Modal 멘트 수정

--- front-end/world-wide-map/src/components/SecondModal.jsx
+++ front-end/world-wide-map/src/components/SecondModal.jsx
@@ -4,7 +4,7 @@ export default function SecondModal() {
   return (
     <div>
       <p style={{fontWeight: "bold"}}>각 나라의 기사들은 지도 아래에서 확인하실 수 있습니다!</p>
-      <p style={{fontWeight: "bold"}}>세계 각국의 소식을 접해보세요.</p>
+      <p style={{fontWeight: "bold"}}>Chat gpt 가 요약해주는 세계 각국의 소식을 접해보세요.</p>
       <img src={miniCardImage} alt="" style={{borderRadius: "10px"}}/>
     </div>
   );

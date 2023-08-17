d94ca03ea47023d42b465d9dbe0c8a08e38f865e
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-08-17 18:05:15 +0900

    fix: 조회수 Top3 RedDot의 maxSize 차이 설정 - resolve #64

--- front-end/world-wide-map/src/pages/WorldMap.jsx
+++ front-end/world-wide-map/src/pages/WorldMap.jsx
@@ -27,50 +27,56 @@ export default function WorldMap() {
   const [hoveredDotId, setHoveredDotId] = useState(null);
   const [redDots, setRedDots] = useState([]);
   const cardsPerPage = 6;
-  
+
   useEffect(() => {
     async function fetchData() {
       const data = await getNewsData();
       setNewsData(data);
     }
-    
+
     fetchData();
   }, []);
-  
+
   // RedDot 설정 시작
   useEffect(() => {
     if (newsData.length > 0) {
       const sortedData = newsData.slice().sort((a, b) => b.visit_count - a.visit_count);
       const top3 = sortedData.slice(0, 3);
-      const dotData = top3.map((item) => ({
-        id: item.id,
-        top: coordsData[item.id - 1].top,
-        left: coordsData[item.id - 1].left,
-        zIndex: item.id,
-        title: item.title,
-        country: item.country,
-        size: item.visit_count * 2,
-      }));
+      const dotData = [];
+
+      for (let index = top3.length - 1; index >= 0; index--) {
+        const item = top3[index];
+        dotData.push({
+          id: item.id,
+          top: coordsData[item.id - 1].top,
+          left: coordsData[item.id - 1].left,
+          zIndex: item.id,
+          title: item.title,
+          country: item.country,
+          size: item.visit_count * 1.8,
+          maxSize: (top3.length - index) * 20
+        });
+      }
       setRedDots(dotData);
     }
   }, [newsData]);
   // RedDot 설정 끝
-  
+
   // 페이지 핸들링 과정 시작
   const totalPageCount = Math.ceil(newsData.length / cardsPerPage);
-  
+
   const handleNextPage = () => {
     setCurrentPage((prevPage) => (prevPage === totalPageCount ? 1 : prevPage + 1));
   };
-  
+
   const handlePrevPage = () => {
     setCurrentPage((prevPage) => (prevPage === 1 ? totalPageCount : prevPage - 1));
   };
-  
+
   const startIndex = (currentPage - 1) * cardsPerPage;
   const endIndex = startIndex + cardsPerPage;
   // 페이징 핸들링 과정 끝
-  
+
   const WorldMapStyle = styled.div`
     display: flex;
     justify-content: center;
@@ -82,7 +88,7 @@ export default function WorldMap() {
     background-position: center;
     background-repeat: repeat;
   `;
-  
+
   const WorldMapImageStyle = styled.div`
     position: relative;
     width: 1200px;
@@ -92,7 +98,7 @@ export default function WorldMap() {
     background-size: cover;
     background-position: center;
   `;
-  
+
   const RedDot = styled.div`
     position: absolute;
     width: ${(props) => Math.min(props.size, props.maxSize)}px; // 최대 크기 제한
@@ -143,7 +149,7 @@ export default function WorldMap() {
       white-space: nowrap;
     }
   `;
-  
+
   const Button = styled.button`
     width: 75px;
     padding: 13px;
@@ -161,34 +167,34 @@ export default function WorldMap() {
       background-color: #8de3dd;
     }
   `;
-  
+
   const CardsContainer = styled.div`
     display: inline-grid;
     grid-template-columns: repeat(3, 1fr);
     row-gap: 20px;
     column-gap: 100px;
   `;
-  
+
   const FlexWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
   `;
-  
+
   const Flex = styled.div`
     display: flex;
     flex-direction: column;
   `;
-  
+
   return (
     <FlexWrapper>
-      
+
       <WorldMapStyle>
-        
+
         <div style={{marginTop: "700px", marginLeft: "100px"}}>
           <Button onClick={handlePrevPage}>이전</Button>
         </div>
-        
+
         <Flex>
           <WorldMapImageStyle>
             {redDots.map((circle) => (
@@ -200,33 +206,33 @@ export default function WorldMap() {
                 size={circle.size}
                 title={circle.title}
                 country={circle.country}
-                maxSize={30}
+                maxSize={circle.maxSize}
                 isHovered={hoveredDotId === circle.id} // hoveredDotId는 state로 관리되는 값
                 onMouseEnter={() => setHoveredDotId(circle.id)}
                 onMouseLeave={() => setHoveredDotId(null)}
               />
             ))}
           </WorldMapImageStyle>
-          
+
           {/*TODO: ?. 옵셔널 체이닝 연산자 사용 시 정상 동작 -> 왜 그런지?*/}
           {/*<h1>최신 업데이트 : {newsData[1].created_at} </h1>*/}
           <hr style={{marginBottom: "30px"}}/>
           <h1 style={{color: "white"}}>최신 업데이트 : {newsData[1]?.created_at.slice(0, 10)} </h1>
           <br/>
           <br/>
-          
+
           <CardsContainer>
             {newsData.slice(startIndex, endIndex).map((data) => (
               <Card key={data.id} data={data}/>
             ))}
           </CardsContainer>
-        
+
         </Flex>
-        
+
         <div style={{marginTop: "700px", marginRight: "100px"}}>
           <Button onClick={handleNextPage}>다음</Button>
         </div>
-      
+
       </WorldMapStyle>
     </FlexWrapper>
   );
